fe1cf0e6e28b9c169b81c58cbd457e2467515360
asilbeek17 <120719876+asilbeek17@users.noreply.github.com>
2023-01-27 13:43:44 +0500

    secons commit in database

--- app/models.py
+++ app/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 from django.urls import reverse
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=155)
+
+
 class Product(models.Model):
-    name = models.CharField(max_length=55)
+    title = models.CharField(max_length=155)
     image = models.ImageField()
     text = models.TextField()
+    price = models.FloatField()
+    quantitiy = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def get_absolute_url(self):
-        return reverse('gallery', args=[str(self.pk)])
+        return reverse('product_detail', args=[str(self.pk)])
+
+
+class Contact(models.Model):
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=13)
+    message = models.TextField()
+
