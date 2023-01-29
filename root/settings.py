3ba9a2e8dfc50e041ebed5e722d1f92d20e8939f
asilbeek17 <120719876+asilbeek17@users.noreply.github.com>
2023-01-29 22:24:25 +0500

    .gitignore

--- root/settings.py
+++ root/settings.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 import os
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xy0(oc_604w=!l(jkckd+z&y5!r7qlw&n+6d@_fkfwlxe3p+ia'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
